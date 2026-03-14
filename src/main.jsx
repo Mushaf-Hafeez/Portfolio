01d41e904ecfdd878acaccfeeafb2c92fcb50cf8
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2026-03-14 12:10:19 +0500

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <>
     <App />
+    <Analytics />
     <ToastContainer theme="dark" autoClose="2000" />
-  </>
+  </>,
 );
